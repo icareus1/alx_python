189b279040d9d9b1c759143ac26e73460234edcc
icareus1 <wamars@live.fr>
2023-07-24 22:46:41 +0000

    Python Import Modules

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,7 +2,6 @@
 from add_0 import add
 if __name__ == "__main__":
     """Print 1 + 2 = 3 Literally by importing a module"""
-    
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
