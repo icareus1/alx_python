a00eb555907a3b957095c269fa713fd40265db26
icareus1 <wamars@live.fr>
2023-07-24 16:59:16 +0000

    Python Import Modules

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,4 +2,8 @@ from add_0 import add
 a = 1
 b = 2
 if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    try:
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    except TypeError:
+        print("Traceback (most recent call last):")
+        print("{} + {}".format(a, b))
