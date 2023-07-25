7cb59a0a994d1e55d82f00d5f2356b90bf61ee64
icareus1 <wamars@live.fr>
2023-07-25 23:58:17 +0000

    Python Data Structures

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,5 +1,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
-            print("{:d}".format(num), end=' ')
+            print("{:d}".format(num))
+            if num[:-1]:
+                end = ' '
         print()
