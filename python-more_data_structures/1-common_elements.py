2738702c46ce86063f1910f4f02458bb20fc8b80
icareus1 <wamars@live.fr>
2023-07-26 20:19:10 +0000

    Python More Data Structures

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,8 +1,10 @@
 def common_elements(set_1, set_2):
     try:
-        # common_ele_set = set(filter(lambda x: x in set_2, set_1))
-        # return common_ele_set
-        common_ele_set = set_1 & set_2
+        common_ele_set = set(filter(lambda x: x in set_2, set_1))
         return common_ele_set
+        # common_ele_set = set_1 & set_2
+        # return common_ele_set
     except TypeError:
+        return None
+    except:
         return None
