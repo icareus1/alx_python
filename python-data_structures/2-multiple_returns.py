b7d16df3941f2cf60bb0d79a4a59ddc7e4bdab6c
icareus1 <wamars@live.fr>
2023-07-26 00:59:32 +0000

    Python Data Structures

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,6 +1,6 @@
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return None
+        return 0,None #parenthesis are optional
     first_char = sentence[0]
     length = len(sentence)
     return (length,first_char)
