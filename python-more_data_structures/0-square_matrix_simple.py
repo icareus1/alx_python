cfc93538863df9fa151d15f432fb0b10e4256fad
icareus1 <wamars@live.fr>
2023-07-26 18:14:49 +0000

    Python More Data Structures

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,17 +1,17 @@
-def square_matrix_simple(matrix=[]):
-    """This is a simpler version of it"""
-    sq_matrix = []
-    for row in matrix:
-        sq_row = []
-        for num in row:
-            sq_row.append(num ** 2)
-        sq_matrix.append(sq_row)
-    return sq_matrix
-
 # def square_matrix_simple(matrix=[]):
-#     """Using lambda and map"""
+#     """This is a simpler version of it"""
 #     sq_matrix = []
 #     for row in matrix:
-#         sq_row = list(map(lambda n: n ** 2, row))
+#         sq_row = []
+#         for num in row:
+#             sq_row.append(num ** 2)
 #         sq_matrix.append(sq_row)
 #     return sq_matrix
+
+def square_matrix_simple(matrix=[]):
+    """Using lambda and map"""
+    sq_matrix = []
+    for row in matrix:
+        sq_row = list(map(lambda n: n ** 2, row))
+        sq_matrix.append(sq_row)
+    return sq_matrix
