bcdaabe56e74e12f4905588beb457dae8f382e40
icareus1 <wamars@live.fr>
2023-08-01 13:10:35 +0000

    Python OOP

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -7,4 +7,4 @@ class Square:
                 raise ValueError("size must be >= 0")
             self.__size = size
         except TypeError:
-            print("size must be an integer")
+            return "size must be an integer"
