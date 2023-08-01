5dd28e425ec8134736771fb8fb99e665a905c098
icareus1 <wamars@live.fr>
2023-08-01 19:24:36 +0000

    Python OOP

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,5 @@
+"""This module defines a simple Square class with a private attribute `size`."""
 class Square:
+    """This class represents a square with a private attribute `size`."""
     def __init__(self, size):
         self.__size = size
