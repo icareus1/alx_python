127a799a927f5a9c3c269e7d8293f54ad62e7b47
icareus1 <wamars@live.fr>
2023-08-01 13:32:18 +0000

    Python OOP

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,7 +4,7 @@ class Square:
     def __init__(self, size = 0):
         if size < 0:
             raise ValueError("size must be >= 0")
-        elif size not isinstance(size, int):
+        elif not isinstance(size, int):
             raise TypeError("size must be an integer")
         else:
             self.__size = size
