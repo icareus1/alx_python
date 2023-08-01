16d374a0b72da511f4e07b2cbdf0534a8ff1bde8
icareus1 <wamars@live.fr>
2023-08-01 19:38:18 +0000

    Python OOP

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -15,7 +15,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            return self.value
             
     def area(self):
         return self.__size ** 2
