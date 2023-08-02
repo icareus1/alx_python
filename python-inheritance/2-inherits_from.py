98c44f9df07a1512c627fc828d6148275ac93582
icareus1 <wamars@live.fr>
2023-08-02 15:07:47 +0000

    Python OOP

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,4 +1,4 @@
 """Check if object is inherited from a class(directly or indirectly)"""
 def inherits_from(obj, a_class):
     """Check if object is inherited from a class(directly or indirectly)"""
-     return isinstance(obj, type) and issubclass(obj, a_class)
+    return isinstance(obj, type) and issubclass(obj, a_class)
