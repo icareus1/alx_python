fa136446f50294fbca06d8f4d26db4f0aefe7cab
icareus1 <wamars@live.fr>
2023-08-02 14:41:08 +0000

    Python OOP

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,7 +1,4 @@
 """Check if object is an instance of a class"""
 def is_same_class(obj, a_class):
     """Check if object is an instance of a class"""
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return isinstance(obj, a_class)
