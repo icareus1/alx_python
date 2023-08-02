70e995e9336b80e5821945bb3d3e8fbd72c1ae35
icareus1 <wamars@live.fr>
2023-08-02 15:00:07 +0000

    Python OOP

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,4 +1,4 @@
 """Check if object is an instance of a sub class"""
 def inherits_from(obj, a_class):
     """Check if object is an instance of a sub class"""
-    return isinstance(obj, type(a_class)) and issubclass(type(obj), a_class)
+    return isinstance(obj, type) or issubclass(type(obj), a_class)
