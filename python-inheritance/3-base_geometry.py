26e49919c03967d3a8f12ff03c862d6c9e9f68f7
icareus1 <wamars@live.fr>
2023-08-03 00:39:09 +0000

    Python OOP

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -2,7 +2,7 @@
 
 class myclass:
     """remove init_subclass from the list of attributes and methods returned"""
-    def __dir__(self):
+    def __dir__(cls) -> None:
         return [attr for attr in super().__dir__() if attr != '__init_subclass__']
 
 class BaseGeometry:
