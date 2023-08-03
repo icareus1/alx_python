53faa7cf5a61cbb346189490caf0ff680c129802
icareus1 <wamars@live.fr>
2023-08-03 12:18:48 +0000

    Python OOP

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,5 +1,9 @@
 """Base Geometry class"""
 class BaseGeometry:
     """"Improved base geometry class"""
+    def __dir__(cls) -> None:
+        attributes = super().__dir__()
+        return [attribute for attribute in attributes if attribute != '__init_subclass__']
+    
     def area(self):
         raise Exception("area() is not implemented")
