796a90c379068925d9c87b0e0cbca054dbfc73e0
icareus1 <wamars@live.fr>
2023-08-03 13:21:06 +0000

    Python OOP

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -7,15 +7,16 @@ class BaseGeometry:
     def __dir__(cls) -> None:
         attributes = super().__dir__()
         return [attribute for attribute in attributes if attribute != '__init_subclass__']
-    
+
     def area(self):
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+
 class Rectangle(BaseGeometry):
     """A class Rectangle that inherits from BaseGeometry and performs some operations"""
     def __init__(self, width, height):
@@ -23,13 +24,13 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-    
+
     def area(self):
         return self.__width * self.__height
 
     def __str__(self):
         return f"[Rectangle] {self.__width}/{self.__height}"
-    
+
 class Square(Rectangle):
     """A class Square that inherits from Rectangle and performs some operations"""
     def __init__(self, size):
