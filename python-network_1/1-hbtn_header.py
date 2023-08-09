ad6caa7d69f4e0cf549bb7adf57853d7845a305a
icareus1 <wamars@live.fr>
2023-08-09 15:13:32 +0000

    Python Network

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,4 +1,10 @@
-
+"""
+    Fetches a URL, sends a request to the URL, and displays the value of the
+    'X-Request-Id' header in the response.
+    The URL is passed as an argument.
+    Note:
+        The value of the 'X-Request-Id' header is different for each request.
+"""
 
 
 import requests, sys
