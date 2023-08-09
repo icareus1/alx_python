3037c11c9368dc2406ac92cf91ac0ded3ab23e6d
icareus1 <wamars@live.fr>
2023-08-09 16:32:26 +0000

    Python Network

---
+++ python-network_1/4-error_code.py
@@ -0,0 +1,15 @@
+"""
+    Fetches a URL, sends a request to the URL, and displays the body of the
+    response. Prints an error message if the HTTP status code is >= 400.
+"""
+
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    res = requests.get(argv[1])
+    print(res.text)
+    if res.status_code >= 400:
+        print(f"Error code: {res.status_code}")
