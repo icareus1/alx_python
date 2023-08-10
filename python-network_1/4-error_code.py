618a86711412b01db37ddffe5324dfa79cbc7fc7
icareus1 <wamars@live.fr>
2023-08-10 20:20:47 +0000

    Python Network

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -9,7 +9,8 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    res = requests.get(sys.argv[1])
-    print(res.text)
+    res = get(sys.argv[1])
     if res.status_code >= 400:
-        print(f"Error code: {res.status_code}")
+        print("Error code: {}".format(res.status_code))
+    else:
+        print(res.text)
