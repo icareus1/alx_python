c56631e81f4670a10b169c518d98ec34b64b58ac
icareus1 <wamars@live.fr>
2023-08-10 20:11:28 +0000

    Python Network

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -15,5 +15,3 @@ if __name__ == '__main__':
     
     if 'X-Request-Id' in res.headers:
         print(res.headers['X-Request-Id'])
-    else:
-        print('No "X-Request-Id" found in the response header')
