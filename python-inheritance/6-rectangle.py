fdcb4d0132231007c6a21e93bbe3f01e30b23c43
icareus1 <wamars@live.fr>
2023-08-13 07:21:20 +0000

    Python Inheritance

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,6 +1,10 @@
 """Base Geometry class"""
-class BaseGeometry:
-    """"Improved base geometry class"""
+class a_metaclass(type):
+    """Override dir"""
+    def __dir__(cls) -> None:
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+class BaseGeometry(metaclass = a_metaclass):
+    """Improved base geometry class""" 
     def __dir__(cls) -> None:
         attributes = super().__dir__()
         return [attribute for attribute in attributes if attribute != '__init_subclass__']
