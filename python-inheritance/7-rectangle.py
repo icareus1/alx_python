3e35a672ae0e4246089ecd6218f3d6be67b3a00b
icareus1 <wamars@live.fr>
2023-08-13 18:38:53 +0000

    Python Inheritance

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,31 +1,29 @@
 """Base Geometry class"""
-
-
-BaseGeometry = __import__("5-base_geometry").BaseGeometry
-# class a_metaclass(type):
-#     """Override dir"""
-#     def __dir__(cls):
-#         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-# class BaseGeometry(metaclass = a_metaclass):
-#     """Improved base geometry class""" 
-#     def __dir__(cls):
-#         attributes = super().__dir__()
-#         return [attribute for attribute in attributes if attribute != '__init_subclass__']
+class a_metaclass(type):
+    """Override dir"""
+    def __dir__(cls) -> None:
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+class BaseGeometry(metaclass = a_metaclass):
+    """Improved base geometry class""" 
+    def __dir__(cls) -> None:
+        attributes = super().__dir__()
+        return [attribute for attribute in attributes if attribute != '__init_subclass__']
     
-#     def area(self):
-#         raise Exception("area() is not implemented")
+    def area(self):
+        raise Exception("area() is not implemented")
     
-#     def integer_validator(self, name, value):
-#         if not isinstance(value, int):
-#             raise TypeError(f"{name} must be an integer")
-#         if value <= 0:
-#             raise ValueError(f"{name} must be greater than 0")
-    
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
 class Rectangle(BaseGeometry):
     """A class Rectangle that inherits from BaseGeometry and perform some operations"""
     def __init__(self, width, height):
-        self.__height = super().integer_validator("width", width)
-        self.__width =  super().integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
     
     def area(self):
         return self.__width * self.__height
