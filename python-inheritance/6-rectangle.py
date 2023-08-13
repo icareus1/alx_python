7d02ff0b30a2246e8491939c4900ff4f9814d97e
icareus1 <wamars@live.fr>
2023-08-13 07:57:54 +0000

    Python Inheritance

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -3,11 +3,13 @@ class a_metaclass(type):
     """Override dir"""
     def __dir__(cls) -> None:
         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-class BaseGeometry(metaclass = a_metaclass):
+
+class BaseGeometry(metaclass=a_metaclass):
     """Improved base geometry class""" 
     def __dir__(cls) -> None:
         attributes = super().__dir__()
         return [attribute for attribute in attributes if attribute != '__init_subclass__']
+
     
     def area(self):
         raise Exception("area() is not implemented")
