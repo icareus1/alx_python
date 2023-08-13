175d94faa817bccbaa5dd935d6b4f98c88089f6c
icareus1 <wamars@live.fr>
2023-08-13 07:15:49 +0000

    Python Inheritance

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,8 +1,11 @@
 """Base Geometry class"""
-class BaseGeometry:
-    """"Empty base geometry class"""
-    # def __dir__(cls) -> None:
-    #     return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+class a_metaclass(type):
+    """Override dir"""
     def __dir__(cls) -> None:
-        attributes = super().__dir__()
-        return [attribute for attribute in attributes if attribute != '__init_subclass__']
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+class BaseGeometry(metaclass = a_metaclass):
+    """"Empty base geometry class"""
+    
+    # def __dir__(cls) -> None:
+    #     attributes = super().__dir__()
+    #     return [attribute for attribute in attributes if attribute != '__init_subclass__']
