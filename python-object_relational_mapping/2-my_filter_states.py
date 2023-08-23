b4c1effbb854a7382b2aadb8bd35cc4c74972e0f
icareus1 <wamars@live.fr>
2023-08-23 12:37:49 +0000

    Python ORM

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+"""
+A script using the module MySQLdb that takes in an argument
+and displays all values in the states from the database
+hbtn_0e_0_usa where name matches the argument 
+"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    name = argv[4]
+    con = MySQLdb.connect(
+                            host='localhost',
+                            port=3306,
+                            user=username,
+                            passwd=password,
+                            db=db_name
+                        )
+    cur = con.cursor()
+    query = f"SELECT * FROM states WHERE name LIKE {name} ORDER BY id ASC"
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    con.close()
