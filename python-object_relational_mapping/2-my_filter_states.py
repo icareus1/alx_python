b835168f2e3290cea7f492a5069ff517ac51b72f
icareus1 <wamars@live.fr>
2023-08-23 12:40:26 +0000

    Python ORM

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
                             db=db_name
                         )
     cur = con.cursor()
-    query = "SELECT * FROM states WHERE name LIKE {} ORDER BY id ASC".format(name)
+    query = "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(name)
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
