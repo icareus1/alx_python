f798951438a46d9480de3dab467f4fc284db87fb
icareus1 <79919003+icareus1@users.noreply.github.com>
2023-08-23 13:15:26 +0000

    Update 3-my_safe_filter_states.py
    Python ORM

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,30 +1,30 @@
-""" 
- A script using the module MySQLdb that takes in an argument 
- and displays all values in the states from the database 
- hbtn_0e_0_usa where name matches the argument 
- """ 
+"""
+A script using the module MySQLdb that takes in an argument
+and displays all values in the states from the database
+hbtn_0e_0_usa where name matches the argument and deal with injection
+ """
+
+
+import MySQLdb
+from sys import argv 
   
-  
- import MySQLdb 
- from sys import argv 
-  
- if __name__ == '__main__': 
-     username = argv[1] 
-     password = argv[2] 
-     db_name = argv[3] 
-     name = argv[4] 
-     con = MySQLdb.connect( 
-                             host='localhost', 
-                             port=3306, 
-                             user=username, 
-                             passwd=password, 
-                             db=db_name 
-                         ) 
-     cur = con.cursor() 
+if __name__ == '__main__':
+     username = argv[1]
+     password = argv[2]
+     db_name = argv[3]
+     name = argv[4]
+     con = MySQLdb.connect(
+                             host='localhost',
+                             port=3306,
+                             user=username,
+                             passwd=password,
+                             db=db_name
+                         )
+     cur = con.cursor()
      query = "SELECT * FROM states WHERE name LIKE BINARY '{}'"
-     cur.execute(query.format(name)) 
-     rows = cur.fetchall() 
-     for row in rows: 
-         print(row) 
-     cur.close() 
+     cur.execute(query.format(name))
+     rows = cur.fetchall()
+     for row in rows:
+         print(row)
+     cur.close()
      con.close()
