cb019d7474894a42d959fe3ccebfbd8568406e2b
icareus1 <wamars@live.fr>
2023-08-24 18:47:16 +0000

    Python ORM

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -13,11 +13,12 @@ Base = declarative_base()
 class State(Base):
     """
     Class representing the 'states' table in the database.
-    
+
     Attributes:
         id (int): An auto-generated unique integer.
         name (str): the name of the state.
     """
     __tablename__ = 'states'
-    id = Column(Integer, autoincrement=True, unique=True, nullable=False, primary_key=True)
+    id = Column(Integer, autoincrement=True,
+                unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
