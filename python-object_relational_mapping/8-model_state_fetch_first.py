bf13ae2b4fa84b2cd4d51b3560af8fc5a5b705a8
icareus1 <wamars@live.fr>
2023-08-24 20:45:27 +0000

    Python ORM

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -21,4 +21,6 @@ if __name__ == "__main__":
     state = session.query(State).order_by(State.id).first()
     if state:
         print(f"{state.id}: {state.name}")
+    else:
+        print("Nothing")
     session.close()
