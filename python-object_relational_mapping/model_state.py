33fe1350df3a7e1deb91f6a00862a51ce60a0be4
icareus1 <wamars@live.fr>
2023-08-24 16:51:57 +0000

    Python ORM

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -17,3 +17,6 @@ class State(Base):
     __tableau__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+    def __init__(self):
+        self.name = name
