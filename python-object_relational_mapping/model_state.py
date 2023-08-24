d8fd4b08942c58371cd3adf7351f66d9d8cb1a59
icareus1 <wamars@live.fr>
2023-08-24 16:31:20 +0000

    Python ORM

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,14 +4,7 @@ Base class, links to the MySQL table states and create rows
 """
 
 
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-from sys import argv
-
-username = argv[1]
-password = argv[2]
-db_name = argv[3]
 
 Base = declarative_base()
 
@@ -20,7 +13,3 @@ class State(Base):
     A class state inheriting Base that is used
     to create a table and insert values in it
     """
-    if __name__== '__main__':
-        __tableau__ = 'states'
-        id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-        name = Column(String(128), nullable=False)
