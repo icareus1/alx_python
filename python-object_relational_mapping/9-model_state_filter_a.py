fb50a0dcd3c976aa353664c386896f5b623894d0
icareus1 <wamars@live.fr>
2023-08-24 20:49:44 +0000

    Python ORM

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states = session.query(State).filter(State.name.like('%a%')).all()
     for state in states:
         print(f"{state.id}: {state.name}")
     session.close()
