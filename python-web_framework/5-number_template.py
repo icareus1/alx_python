fb2b73709a3ca33639381f650cabf3cd60087429
icareus1 <wamars@live.fr>
2023-08-28 17:58:02 +0000

    Python - Flask

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -41,7 +41,7 @@ def number(n):
     return f'{n} is a number'
 
 
-@app.route('/number_template/<int:n>')
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     return render_template('5-number.html', number=n)
 
