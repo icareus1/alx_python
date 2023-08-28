b1bdf6c8a6799ebba75c344d266c69014fafb64a
icareus1 <wamars@live.fr>
2023-08-28 19:31:50 +0000

    Python - Flask

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -43,7 +43,7 @@ def number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return render_template('5-number.html', number=n)
+    return render_template('5-number.html', n=n)
 
 
 @app.errorhandler(404)
