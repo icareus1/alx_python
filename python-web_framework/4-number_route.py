d232abdbb41f0b2743eb3a349c8c2dc04903f996
icareus1 <wamars@live.fr>
2023-08-28 12:59:58 +0000

    Python -Flask

---
+++ python-web_framework/4-number_route.py
@@ -0,0 +1,47 @@
+"""Script that starts a Flask web application & displays a value"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    ftext = text.replace('_', ' ')
+    return f'C {ftext}'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def the_p_snake(text=None):
+    if text is None:
+        text = 'is cool'
+    try:
+        ftext = text.replace('_', ' ')
+        return f'Python {ftext}'
+    except Exception as e:
+        return 'Python is cool'
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    if isinstance(n, int):
+        return n
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return "Python is cool", 404
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
