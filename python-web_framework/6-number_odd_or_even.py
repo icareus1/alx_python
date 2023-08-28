5ce04b7cc27ce196adce4dd8898185ddead1a575
icareus1 <wamars@live.fr>
2023-08-28 23:54:11 +0000

    Python - Flask

---
+++ python-web_framework/6-number_odd_or_even.py
@@ -0,0 +1,61 @@
+"""
+Script that starts a Flask web application & displays a value
+Odd or Even value.
+"""
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    ftext = text.replace('_', ' ')
+    return f'C {ftext}'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def the_p_snake(text=None):
+    if text is None:
+        text = 'is cool'
+    try:
+        ftext = text.replace('_', ' ')
+        return f'Python {ftext}'
+    except Exception as e:
+        return 'Python is cool'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    if isinstance(n, int):
+        odd_or_even = "odd" if n % 2 else "even"
+        return render_template('6-number_odd_or_even.html', n=n, odd_even=odd_or_even)
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return "Python is cool", 404
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
