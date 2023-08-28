b98676244b3655b6cc6be627b47617e2e1840d0c
icareus1 <wamars@live.fr>
2023-08-28 12:44:33 +0000

    Python -Flask

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -32,5 +32,11 @@ def the_p_snake(text=None):
     except Exception as e:
         return 'Python is cool'
 
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return "Python is cool"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
