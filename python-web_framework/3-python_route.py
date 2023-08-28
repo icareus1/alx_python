75f0f6e73263c02a714832a7eb08ae14b038a4d4
icareus1 <wamars@live.fr>
2023-08-28 10:33:54 +0000

    Python -Flask

---
+++ python-web_framework/3-python_route.py
@@ -0,0 +1,32 @@
+"""Script that starts a Flask web application & displays a value(str)"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    ftext = text.replace('_', ' ')
+    return f'C {ftext}'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def the_p_snake(text):
+    text = 'is cool'
+    ftext = text.replace('_', ' ')
+    return f'Python {ftext}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
