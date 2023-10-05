bea994f90d989d84584634241a40bf6182ceb0ec
icareus1 <wamars@live.fr>
2023-10-05 15:30:20 +0000

    API

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 import csv
-import os
+# import os
 import requests
 import sys
 
@@ -63,10 +63,10 @@ def main():
     emp_name = get_employee_name(emp_id)
     csv_filename = f"{emp_id}.csv"
     
-    # Check if the CSV file already exists
-    if os.path.exists(csv_filename):
-        print(f"Data already exported to {csv_filename}")
-        return
+    # # Check if the CSV file already exists
+    # if os.path.exists(csv_filename):
+    #     print(f"Data already exported to {csv_filename}")
+    #     return
 
     # Open the CSV file for writing
     with open(csv_filename, mode='w', newline='') as csv_file:
